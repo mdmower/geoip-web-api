9e23d5a8f2b151e489d5f449da9e93f58c2533ab
Matt Mower <mdmower@cmphys.com>
2025-03-29 13:00:51 -0700

    Use projectService for ts-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -31,7 +31,7 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         sourceType: 'module',
-        project: true,
+        projectService: true,
       },
     },
     rules: {
